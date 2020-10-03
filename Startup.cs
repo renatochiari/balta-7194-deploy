e80b73d50e42814c3c7eac8978c78b8e08c13f11
Renato Chiari <renatochiari_15@hotmail.com>
2020-10-03 11:12:52 -0300

    Atualização

--- Startup.cs
+++ Startup.cs
@@ -62,8 +62,8 @@ namespace Shop
                 };
             });
 
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
-            //services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
+            //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
         
             services.AddSwaggerGen(c =>
             {
